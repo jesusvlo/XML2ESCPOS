9cb526587a829a3eab47403107658c659561ab43
Jesus <jesus.villa@hotmail.es>
2022-01-10 11:08:59 +0100

    Version NetStd 2.0

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XML2ESCPOS
+{
+    class Class1
+    {
+        Class1()
+        {
+            
+        }
+
+    }
+}
